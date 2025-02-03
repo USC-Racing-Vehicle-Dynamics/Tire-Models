aff03f3fd2fbaa6c31cdf1d900a46664dc9e1ae7
lewistple <tianpail@usc.edu>
2025-02-03 10:07:16 -0800

    Update tireCoefficient.m

--- tireCoefficient.m
+++ tireCoefficient.m
@@ -20,12 +20,12 @@ IA_deg = 2;
 IA_rad = deg2rad(IA_deg);
 
 % Normal load (positive -z)
-FZ_lb = 250; 
-FZ_N = FZ_lb / 0.224809;
+FZ_lb = 200; 
+FZ_N = FZ_lb * 4.448;
 
 % Slip angle
-SA_deg = -18: 0.5: 18;
-SA_rad = deg2rad(SA_deg);
+SA_deg_space = -18: 0.5: 18;
+SA_rad_space = deg2rad(SA_deg_space);
 
 % UI Table
 rowCount = 1;
@@ -34,24 +34,32 @@ rowCount = 1;
 
 % Reference tire model: needs to have both cornering and drive/brake testing data
 % Model: Hoosier 43100 R20 18''x6''
-tire_fx_ref = load('Fitted Data/43100_R20_18x6_FX0.mat');
 tire_fy_ref = load('Fitted Data/43100_R20_18x6_FY0.mat');
 
-% %% Vector Spaces
-% 
-% % Lateral force, FY [lbf]
-% FY_R20 = zeros(length(SA_rad), 1);
-% FY_LCO = zeros(length(SA_rad), 1);
-% FY_R25B = zeros(length(SA_rad), 1);
-% FY_R20n = zeros(length(SA_rad), 1);
-% 
-% FYSA_R20 = zeros(2, length(SA_rad));
-% FYSA_LCO = zeros(2, length(SA_rad));
-% FYSA_R25B = zeros(2, length(SA_rad));
-% FYSA_R20n = zeros(2, length(SA_rad));
+% Target tire model
+% Model: Hoosier 43075 R20 16''x7.5''
+tire_fy = load('Fitted Data/43075_R20_16x7.5_FY0.mat');
 
 %%
 
-% tire = load('Run Data/RunData_DriveBrake_Matlab_SI_Round9/B2356run72.mat');
-% dataPts = length(tire.ET);
-% plot(tire.SL, tire.FX);
+FYR = [];
+FY = [];
+for i = 1: length(SA_rad_space)
+
+    SA_rad = SA_rad_space(i);
+
+    % Lateral force of reference tire
+    [~, fyr] = magicformula(tire_fy_ref.mfparams, 0, SA_rad, FZ_N, IP_pa, IA_rad); 
+    FYR = [FYR fyr];
+
+    % Lateral force of target tire
+    [~, fy] = magicformula(tire_fy.mfparams, 0, SA_rad, FZ_N, IP_pa, IA_rad); 
+    FY = [FY fy];
+
+end
+cof_rmax = max(FYR / FZ_N);
+cof_rmin = min(FYR / FZ_N);
+cof_max = max(FY / FZ_N);
+cof_min = min(FY / FZ_N);
+
+coeff = ((cof_max/cof_rmax) + (cof_min/cof_rmin))/2;
